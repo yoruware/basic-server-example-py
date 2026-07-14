1e7dcf7f08dbbe567e96c6bf28d0ce258f53b008
yoruware <betulsakrak45@gmail.com>
2026-07-14 23:30:32 +0300

    Edit comments in server.py
    Updated comments in server.py for clarity.

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 import socket
-import subprocess #clientın serverdan talep ettiği komutları çalıştırabilmemiz için eklediğimiz kütüphane
+import subprocess #The library we added allows us to execute the commands that the client requests from the server.
 
 host = "127.0.0.1"
 port = 50001
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_TEAM)
 server_socket.bind((host,port))
-server_socket.listen()  #önce dinle ve gelen mesajı al
+server_socket.listen()  #listen and get message
 
 conn, addr = server_socket.accept()
 print("connected from : "+str(addr))
@@ -16,13 +16,13 @@ while True:
     print(data)
 
     result = subprocess.run(data, stdout=subprocess.PIPE, shell=True)  
-    # ! subprocess.run() komut terminalini çalıştırıyor.
-    # stdout=subprocess.PIPE >> komutun çıktısını al, terminale yazdırma demektir.
+    # ! subprocess.run() runs command terminal.
+    # stdout=subprocess.PIPE >> Get the output of the command but don't print it.
 
-    # shell=True >>  komutları kabuk (shell) üzerinden çalıştır 
+    # shell=True >>  Run commands through the shell.
 
     if(result.stdout.decode()!=""):
-        response_data = result.stdout #çıktı
+        response_data = result.stdout #output
     else:
         response_data = ("Komut Calıstırıldı").encode()
 
